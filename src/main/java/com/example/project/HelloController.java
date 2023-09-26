322624c7dd135685941da383844be1a47aeead69
dmytro-dontsov <qemmnes@gmail.com>
2023-09-26 13:11:21 +0200

    batton click processing

--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -1,14 +1,32 @@
 package com.example.project;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    private char nowSym = 'x';
+
+    @FXML
+    void btnClick(ActionEvent event) {
+        ((Button)event.getSource()).setText(String.valueOf(nowSym));
+        nowSym = nowSym == 'x' ? '0' : 'x';
     }
+
+    @FXML
+    void initialize() {
+
+    }
+
 }
+
+
