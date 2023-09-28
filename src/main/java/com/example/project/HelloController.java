4f8ef3646af129ef0a2f0505761174976c7c503d
dmytro-dontsov <qemmnes@gmail.com>
2023-09-28 15:46:02 +0200

    the key pat

--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.project;
 
 import java.net.URL;
+import java.util.Calendar;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
 
@@ -16,10 +20,43 @@ public class HelloController {
 
     private char nowSym = 'x';
 
+    private char gameField[][] = new char[3][3];
+
+    private boolean isGame = true;
+
     @FXML
     void btnClick(ActionEvent event) {
-        ((Button)event.getSource()).setText(String.valueOf(nowSym));
-        nowSym = nowSym == 'x' ? '0' : 'x';
+        Button btn = (Button)event.getSource();
+
+        if (!isGame || btn.getText() != "") return;
+
+        int rowIndex = GridPane.getRowIndex(btn) == null ? 0 : GridPane.getRowIndex(btn);
+        int columnIndex = GridPane.getColumnIndex(btn) == null ? 0 : GridPane.getColumnIndex(btn);
+
+        gameField[rowIndex][columnIndex] = nowSym;
+
+        btn.setText(String.valueOf(nowSym));
+
+        if (gameField[0][0] == gameField[0][1] && gameField[0][0] == gameField[0][2] && (gameField[0][0] == 'x' || gameField[0][0] == 'o')) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "We have a winner \"" + btn.getText() + "\"", ButtonType.OK);
+            alert.showAndWait();
+            isGame = false;
+        } else if (gameField[1][0] == gameField[1][1] && gameField[1][0] == gameField[1][2] && (gameField[1][0] == 'x' || gameField[1][0] == 'o')) {
+            isGame = false;
+        } else if (gameField[2][0] == gameField[2][1] && gameField[2][0] == gameField[2][2] && (gameField[2][0] == 'x' || gameField[2][0] == 'o')) {
+            isGame = false;
+        } else if (gameField[0][0] == gameField[1][0] && gameField[0][0] == gameField[2][0] && (gameField[0][0] == 'x' || gameField[0][0] == 'o')) {
+            isGame = false;
+        } else if (gameField[0][1] == gameField[1][1] && gameField[0][1] == gameField[2][1] && (gameField[0][1] == 'x' || gameField[0][1] == 'o')) {
+            isGame = false;
+        } else if (gameField[0][2] == gameField[1][2] && gameField[0][2] == gameField[2][2] && (gameField[0][2] == 'x' || gameField[0][2] == 'o')) {
+            isGame = false;
+        } else if (gameField[0][0] == gameField[1][1] && gameField[0][0] == gameField[2][2] && (gameField[0][0] == 'x' || gameField[0][0] == 'o')) {
+            isGame = false;
+        } else if (gameField[2][0] == gameField[1][1] && gameField[2][0] == gameField[0][2] && (gameField[2][0] == 'x' || gameField[2][0] == 'o')) {
+            isGame = false;
+        }
+        nowSym = nowSym == 'x' ? 'o' : 'x';
     }
 
     @FXML
